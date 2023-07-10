830987ccda484f5c30590dadf5a8be8503f8b791
AlmirFonseca <almirfonseca60@gmail.com>
2023-07-10 13:38:07 -0300

    Update the comments in the Network header file

--- mainSketch/Network.h
+++ mainSketch/Network.h
@@ -6,25 +6,25 @@
 // Define the WiFi credentials (https://youtu.be/pDDdA9qEFwY)
 static const char* WIFI_SSID = "Cadeirudo";
 static const char* WIFI_PASSWORD = "cadeirinha123*#";
-// #define WIFI_SSID "Cadeirudo"
-// #define WIFI_PASSWORD "cadeirinha123*#"
 
 // Define constants for the connection with the NTP Server, in order to get the current time
 static const char* ntpServer = "pool.ntp.org";  // NTP Server address
 static const long gmtOffset_sec = -10800;  // GMT Offset in seconds (-3 hours)
 static const int daylightOffset_sec = 0;  // Daylight Offset in seconds (0)
-// #define ntpServer "pool.ntp.org"
-// #define gmtOffset_sec -10800
-// #define daylightOffset_sec 0
 
+// Setup the WiFi connection
 void setupWiFi();
 
+// Sync the device's time with an NTP Server time
 void syncWithNTPTime();
 
+// Prints the time obtained from the NTP server during the initialization
 void printLocalTime();
 
+// Function that gets current epoch time
 time_t getCurrentTime();
 
+// Function that return the current timestamp in milliseconds since 01 January 1970
 unsigned long long getCurrentMillisTimestamp();
 
 #endif  // Network_H_
