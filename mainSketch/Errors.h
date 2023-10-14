809013e061e0662135a29d06cae4a26f7eec1e31
gustavorrocha <gustavoreisrocha.grr@gmail.com>
2023-10-14 15:02:47 -0300

    Update doc for error class

--- mainSketch/Errors.h
+++ mainSketch/Errors.h
@@ -20,7 +20,7 @@
  * noNTPdata: NTP Sync failed (Magenta)
  * bufferFull: Buffer full (Red)
  * externalADCInitFailure: External ADCs initialization failure (Aqua)
-*/
+ */
 enum class ErrorType {
 	None,
 	NoInternet,
@@ -30,12 +30,27 @@ enum class ErrorType {
 	ExternalADCInitFailure
 };
 
+/**
+ * This class handle the errors that may occur during the execution of the sketch.
+ * In case of an error, the RGB built-in LED will change its color to indicate the error type.
+ * If the error is fatal, the device will be restarted in 3 seconds, in order to try to recover from the error.
+ */
 class Errors {
 public:
 	/** Initialize the RGB built-in LED */
 	Errors();
 
-	/** Update the LED color according to the current error status */
+	/** Update the LED color according to the current error status: 
+	 * - If no error, the LED will be green (none)
+	 * - If there is no internet connection, the LED will be yellow (noInternet)
+	 * - If there is no database connection, the LED will be dark blue (noDatabaseConnection)
+	 * - If the NTP sync failed, the LED will be magenta (noNTPdata)
+	 * - If the buffer is full, the LED will be red (bufferFull)
+	 * - If the external ADCs initialization failed, the LED will be aqua (externalADCInitFailure)
+	 * 
+	 * @param error The error type
+	 * @param fatal If true, the device will be restarted in 3 seconds
+	*/
 	void showError(ErrorType error, bool fatal = false);
 
 	/** Update the LED brightness */
