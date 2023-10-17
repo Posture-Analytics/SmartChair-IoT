45b431a2d1cc5c88fdcce611d3d94dbe1a230b33
AlmirFonseca <almirfonseca60@gmail.com>
2023-10-17 12:01:00 -0300

    Improve the debug module, turning into something more robust and closer to the usual 'Serial.print'

--- mainSketch/Debug.h
+++ mainSketch/Debug.h
@@ -1,24 +1,25 @@
 /* Debug.h
 
-    * This module handle the debug messages that may occur during the execution of the sketch.
-    * It also add or remove sensors from the list of sensors to be read, turn on or off funcionalities, etc.
+    * This module handles the debug messages that may occur during the execution of the sketch.
 
     TODO:
-    * The debug messages are sent to the serial port, but it defines an routine to send logs to the database as well.
+    * (Only apply) It also adds or removes sensors from the list of sensors to be read, turn on or off functionalities, etc.
+    * The debug messages are sent to the serial port, but it defines a routine to send logs to the database as well.
 
 */
 
 #ifndef Debug_H_
 #define Debug_H_
 
+#include <Arduino.h>
+
 #define DEBUG_LEVEL_NONE                0 // No messages
 #define DEBUG_LEVEL_FATAL               1 // Fatal errors
 #define DEBUG_LEVEL_ERROR               2 // All errors
 #define DEBUG_LEVEL_WARNING             3 // Errors and warnings
-#define DEBUG_LEVEL_INFO                4 // Errors, warnings and info
-#define DEBUG_LEVEL_DEBUG               5 // Errors, warnings, info and debug
-#define DEBUG_LEVEL_TRACE               6 // Errors, warnings, info, debug and trace (step by step execution)
-#define DEBUG_LEVEL_VERBOSE             7 // All
+#define DEBUG_LEVEL_INFO                4 // Errors, warnings, and info
+#define DEBUG_LEVEL_DEBUG               5 // Errors, warnings, info, and debug
+#define DEBUG_LEVEL_VERBOSE             7 // All (Errors, warnings, info, debug, and trace (step by step execution))
 
 #define DISABLE                         0 // Disable the sensor/functionality
 #define ENABLE                          1 // Enable the sensor/functionality
@@ -33,12 +34,12 @@
 #define DISTANCE_MATRIX_SENSOR_STATUS   DISABLE
 #define IMU_SENSOR_STATUS               DISABLE
 
-// Set the status of the funcionalities (ENABLE_SENSOR or DISABLE_SENSOR)
+// Set the status of the functionalities (ENABLE_SENSOR or DISABLE_SENSOR)
 #define WIFI_STATUS                     ENABLE
 #define NTP_STATUS                      ENABLE
 #define DATABASE_STATUS                 ENABLE
 
-const char *debugLevelLabels[] = {
+static const char *debugLevelLabels[] = {
     "",
     "FATAL",
     "ERROR",
@@ -53,68 +54,73 @@ const char *debugLevelLabels[] = {
 
 #define DEBUG_ENABLED 1
 
-#define LOG_LEVEL(level, format, ...)          \
-    if (level <= DEBUG_LEVEL)                  \
-    {                                          \
-        Serial.print(debugLevelLabels[level]); \
-        Serial.print(": ");                    \
-        Serial.print(format, ##__VA_ARGS__);   \
+template <typename T>
+void logArgs(const T& t) {
+    Serial.print(t);
+}
+
+template <typename T, typename... Args>
+void logArgs(const T& t, const Args&... args) {
+    Serial.print(t);
+    logArgs(args...);
+}
+
+#define LOG_LEVEL(level, ...)                   \
+    if (level <= DEBUG_LEVEL)                   \
+    {                                           \
+        Serial.print(debugLevelLabels[level]);  \
+        Serial.print(": ");                     \
+        logArgs(__VA_ARGS__);                   \
     }
 
-#define LOG_LEVEL_LN(level, format, ...)       \
-    if (level <= DEBUG_LEVEL)                  \
-    {                                          \
-        Serial.print(debugLevelLabels[level]); \
-        Serial.print(": ");                    \
-        Serial.print(format, ##__VA_ARGS__);   \
-        Serial.println();                      \
+#define LOG_LEVEL_LN(level, ...)                \
+    if (level <= DEBUG_LEVEL)                   \
+    {                                           \
+        Serial.print(debugLevelLabels[level]);  \
+        Serial.print(": ");                     \
+        logArgs(__VA_ARGS__);                   \
+        Serial.println();                       \
     }
 
-#define LogFatal(format, ...) LOG_LEVEL(DEBUG_LEVEL_FATAL, format, ##__VA_ARGS__)
-#define LogFatalLn(format, ...) LOG_LEVEL_LN(DEBUG_LEVEL_FATAL, format, ##__VA_ARGS__)
+#define LogFatal(...) LOG_LEVEL(DEBUG_LEVEL_FATAL, __VA_ARGS__)
+#define LogFatalln(...) LOG_LEVEL_LN(DEBUG_LEVEL_FATAL, __VA_ARGS__)
 
-#define LogError(format, ...) LOG_LEVEL(DEBUG_LEVEL_ERROR, format, ##__VA_ARGS__)
-#define LogErrorLn(format, ...) LOG_LEVEL_LN(DEBUG_LEVEL_ERROR, format, ##__VA_ARGS__)
+#define LogError(...) LOG_LEVEL(DEBUG_LEVEL_ERROR, __VA_ARGS__)
+#define LogErrorln(...) LOG_LEVEL_LN(DEBUG_LEVEL_ERROR, __VA_ARGS__)
 
-#define LogWarning(format, ...) LOG_LEVEL(DEBUG_LEVEL_WARNING, format, ##__VA_ARGS__)
-#define LogWarningLn(format, ...) LOG_LEVEL_LN(DEBUG_LEVEL_WARNING, format, ##__VA_ARGS__)
+#define LogWarning(...) LOG_LEVEL(DEBUG_LEVEL_WARNING, __VA_ARGS__)
+#define LogWarningln(...) LOG_LEVEL_LN(DEBUG_LEVEL_WARNING, __VA_ARGS__)
 
-#define LogInfo(format, ...) LOG_LEVEL(DEBUG_LEVEL_INFO, format, ##__VA_ARGS__)
-#define LogInfoLn(format, ...) LOG_LEVEL_LN(DEBUG_LEVEL_INFO, format, ##__VA_ARGS__)
+#define LogInfo(...) LOG_LEVEL(DEBUG_LEVEL_INFO, __VA_ARGS__)
+#define LogInfoln(...) LOG_LEVEL_LN(DEBUG_LEVEL_INFO, __VA_ARGS__)
 
-#define LogDebug(format, ...) LOG_LEVEL(DEBUG_LEVEL_DEBUG, format, ##__VA_ARGS__)
-#define LogDebugLn(format, ...) LOG_LEVEL_LN(DEBUG_LEVEL_DEBUG, format, ##__VA_ARGS__)
+#define LogDebug(...) LOG_LEVEL(DEBUG_LEVEL_DEBUG, __VA_ARGS__)
+#define LogDebugln(...) LOG_LEVEL_LN(DEBUG_LEVEL_DEBUG, __VA_ARGS__)
 
-#define LogTrace(format, ...) LOG_LEVEL(DEBUG_LEVEL_TRACE, format, ##__VA_ARGS__)
-#define LogTraceLn(format, ...) LOG_LEVEL_LN(DEBUG_LEVEL_TRACE, format, ##__VA_ARGS__)
-
-#define LogVerbose(format, ...) LOG_LEVEL(DEBUG_LEVEL_VERBOSE, format, ##__VA_ARGS__)
-#define LogVerboseLn(format, ...) LOG_LEVEL_LN(DEBUG_LEVEL_VERBOSE, format, ##__VA_ARGS__)
+#define LogVerbose(...) LOG_LEVEL(DEBUG_LEVEL_VERBOSE, __VA_ARGS__)
+#define LogVerboseln(...) LOG_LEVEL_LN(DEBUG_LEVEL_VERBOSE, __VA_ARGS__)
 
 #else
 
 #define DEBUG_ENABLED 0
 
-#define LogFatal(format, ...)
-#define LogFatalLn(format, ...)
+#define LogFatal(...)
+#define LogFatalln(...)
 
-#define LogError(format, ...)
-#define LogErrorLn(format, ...)
+#define LogError(...)
+#define LogErrorln(...)
 
-#define LogWarning(format, ...)
-#define LogWarningLn(format, ...)
+#define LogWarning(...)
+#define LogWarningln(...)
 
-#define LogInfo(format, ...)
-#define LogInfoLn(format, ...)
+#define LogInfo(...)
+#define LogInfoln(...)
 
-#define LogDebug(format, ...)
-#define LogDebugLn(format, ...)
+#define LogDebug(...)
+#define LogDebugln(...)
 
-#define LogTrace(format, ...)
-#define LogTraceLn(format, ...)
-
-#define LogVerbose(format, ...)
-#define LogVerboseLn(format, ...)
+#define LogVerbose(...)
+#define LogVerboseln(...)
 
 #endif // DEBUG_LEVEL
 
